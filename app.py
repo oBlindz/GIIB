6ad4c61f8db51f4d75b24a04d7c10a148ef551be
oBlindz <kakeguruiso2@gmail.com>
2026-04-24 09:16:07 -0300

    End: Finished the base idea, all works well, but without validations or tests

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Image size: 1536px, 2048px
 from PIL import Image, ImageDraw, ImageFont 
 
 def receive_inputs_from_terminal():
@@ -15,16 +16,22 @@ def processing_match_stats(name, kills, assists, deaths, rounds_win, rounds_lose
     kpr     = round(kills/(rounds_win+rounds_lose),2)
     rating  = round(((kdr*0.5)+(kpr-dpr))/(rounds_win+rounds_lose),2)
 
-    insert_text_in_image("./image/image.png",name)
+    insert_text_in_image("./image/image.png",name,kdr,kpr,dpr,rating)
 
-def insert_text_in_image(imagePath, nickname):
+def insert_text_in_image(imagePath, nickname,kdr,kpr,dpr,rtg):
     with Image.open(imagePath) as im:
-        draw    = ImageDraw.Draw(im)
+        draw            = ImageDraw.Draw(im)
+        nickname_font   = ImageFont.truetype("./Roboto/Roboto-VariableFont_wdth,wght.ttf",110)
+        stats_font      = ImageFont.truetype("./Roboto/Roboto-VariableFont_wdth,wght.ttf",80)
+        rating_font     = ImageFont.truetype("./Roboto/Roboto-VariableFont_wdth,wght.ttf",110)
 
-        draw.text((1536//2,200),nickname,font=font,fill=(255,255,255))
+        draw.text((250,250),nickname,font=nickname_font,fill=(255,255,255))
+        draw.text((800,650),f"KDR: {kdr}",font=stats_font,fill=(255,255,255))
+        draw.text((800,920),f"KPR: {kpr}",font=stats_font,fill=(255,255,255))
+        draw.text((800,1180),f"DPR: {dpr}",font=stats_font,fill=(255,255,255))        
+        draw.text((620,1750),f"RATING: {rtg}",font=rating_font,fill=(255,255,255))
 
         im.save("./out/image.png")
         im.show()
 
 receive_inputs_from_terminal()
-# Image size:1536,2048
